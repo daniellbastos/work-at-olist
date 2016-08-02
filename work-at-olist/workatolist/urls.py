e3a3f449f325691b97aef58425cf2bc9bae53140
Daniel Bastos <danielfloresbastos@gmail.com>
2016-08-02 13:59:58 -0300

    Page with info of project and personal data.

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='channels/home.html')),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('apps.channels.urls')),
     url(r'^admin/', admin.site.urls),
