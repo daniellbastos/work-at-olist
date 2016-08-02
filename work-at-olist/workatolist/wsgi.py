a3dfe14ac32ed852272e0932760897a54235ff9e
Daniel Bastos <danielfloresbastos@gmail.com>
2016-08-02 11:15:37 -0300

    Server static files with whitenoise.

--- work-at-olist/workatolist/wsgi.py
+++ work-at-olist/workatolist/wsgi.py
@@ -12,8 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from prettyconf import config
+from whitenoise.django import DjangoWhiteNoise
 
 settings = config('SETTINGS', default='workatolist.settings.heroku')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
